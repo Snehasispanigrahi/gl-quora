7595cc227f4623869bbc8a589ab81370aee81ef3
Snehashish Panigrahi <panigrahisnehasis@gmail.com>
2019-03-16 20:26:56 +0530

    Removed auto-security for testing purpose

--- src/main/java/com/greatlearning/quora/QuoraApplication.java
+++ src/main/java/com/greatlearning/quora/QuoraApplication.java
@@ -2,12 +2,13 @@ package com.greatlearning.quora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class QuoraApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QuoraApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(QuoraApplication.class, args);
+    }
 
 }
